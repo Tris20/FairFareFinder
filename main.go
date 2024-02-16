a4848d22952e387c585accd02b7634ed135bc38b
jared <key.jared.a@gmail.com>
2024-02-16 18:24:46 +0100

    beauty

--- main.go
+++ main.go
@@ -60,6 +60,10 @@ func main() {
 		http.HandleFunc("/forecast", forecastHandler)
 		http.HandleFunc("/getforecast", getForecastHandler)
 
+		// Serve static files from the `images` directory
+		fs := http.FileServer(http.Dir("src/images"))
+		http.Handle("/images/", http.StripPrefix("/images/", fs))
+
 		// Start the web server
 		fmt.Println("Starting server on :8080")
 		if err := http.ListenAndServe(":8080", nil); err != nil {
