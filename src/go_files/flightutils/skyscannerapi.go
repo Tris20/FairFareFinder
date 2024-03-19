757f230f77705b4b0c9d074a8ac5d358114f347f
Tristan <tris.robinson90@gmail.com>
2024-03-20 00:22:39 +0100

    DEV: DB cleanup

---
+++ src/go_files/flightutils/skyscannerapi.go
@@ -0,0 +1,32 @@
+/*
+package flightutils
+
+import (
+	"encoding/json"
+	"fmt"
+  "net/url"
+	"github.com/Tris20/FairFareFinder/src/go_files"
+	"github.com/Tris20/FairFareFinder/src/go_files/config_handlers"
+)
+
+
+
+func SearchOneWay()
+
+
+
+
+func GetLowestPriceForLocation(origin, destination)
+
+
+
+
+
+func GetFlightDates(origin, destination)
+//Search flights.db for origin to destination W TH FR SA 
+
+//Search flights.db for origin to destination SU Mon Tue Wed 
+
+*/
+
+
