5318389498ba5b932c2e928df81ef488ba57c666
Tristan <tris.robinson90@gmail.com>
2024-05-24 22:17:03 +0200

    PERF: significant speed increase for populating weather table of results.db

--- utils/database_utils/results/dbinsert.go
+++ utils/database_utils/results/dbinsert.go
@@ -16,21 +16,40 @@ func InsertWeatherData(destinationDBPath string, records []WeatherRecord) error 
 	}
 	defer db.Close()
 
-	insertQuery := `
+	// Start a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	// Prepare the insert statement
+	stmt, err := tx.Prepare(`
 		INSERT INTO weather (city_name, country_code, date, weather_type, temperature, weather_icon_url, google_weather_link)
 		VALUES (?, ?, ?, ?, ?, ?, ?)
-	`
+	`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
 	// Create a new progress bar
 	bar := progressbar.Default(int64(len(records)))
 
 	for _, record := range records {
-		_, err := db.Exec(insertQuery, record.CityName, record.CountryCode, record.Date, record.WeatherType, record.Temperature, record.WeatherIconURL, record.GoogleWeatherLink)
+		_, err := stmt.Exec(record.CityName, record.CountryCode, record.Date, record.WeatherType, record.Temperature, record.WeatherIconURL, record.GoogleWeatherLink)
 		if err != nil {
+			// Rollback the transaction in case of an error
+			tx.Rollback()
 			return err
 		}
 		// Increment the progress bar
 		bar.Add(1)
+	}
+
+	// Commit the transaction
+	err = tx.Commit()
+	if err != nil {
+		return err
 	}
 
 	return nil
