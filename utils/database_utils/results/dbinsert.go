0471b625f732094166405266f24687e83ad02a79
Tristan <tris.robinson90@gmail.com>
2024-05-28 20:59:42 +0200

    FEATURE: Avg daily weather and Location_Avg WPI now calculated for resultsdb

--- utils/database_utils/results/dbinsert.go
+++ utils/database_utils/results/dbinsert.go
@@ -61,28 +61,13 @@ func InsertWeatherData(tablename string, destinationDBPath string, records []Wea
 }
 
 
-// InsertLocationData inserts unique location records into the location table in results.db
-func InsertLocationData(destinationDBPath string, records []WeatherRecord) error {
+
+func InsertLocationData(destinationDBPath string, uniqueLocations []Location) error {
 	db, err := sql.Open("sqlite3", destinationDBPath)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
-
-	// Extract unique locations
-	uniqueLocations := getUniqueLocations(records)
-
-	// Collect unique IATA codes
-	iataCodes := make([]string, len(uniqueLocations))
-	for i, loc := range uniqueLocations {
-		iataCodes[i] = loc.IATA
-	}
-
-	// Fetch all SkyScanner IDs at once
-	skyscannerIDs, err := FetchAllSkyScannerIDs(iataCodes)
-	if err != nil {
-		return err
-	}
 
 	// Start a transaction
 	tx, err := db.Begin()
@@ -104,18 +89,6 @@ func InsertLocationData(destinationDBPath string, records []WeatherRecord) error
 	bar := progressbar.Default(int64(len(uniqueLocations)))
 
 	for _, loc := range uniqueLocations {
-		// Generate URLs for flights and hotels
-		airbnbURL := GenerateAirbnbURL(loc.CityName)
-		bookingURL := GenerateBookingURL(loc.CityName)
-    //calcualte avg wpi here
-    temp_five_day_wpi, _ :=  ProcessLocation(loc, records)
-    loc.FiveDayWPI = temp_five_day_wpi 
-		loc.AirbnbURL = airbnbURL
-		loc.BookingURL = bookingURL
-
-		// Fetch skyscanner ID from the map
-		loc.SkyScannerID = skyscannerIDs[loc.IATA]
-
 		_, err = stmt.Exec(loc.CityName, loc.CountryCode, loc.IATA, loc.AirbnbURL, loc.BookingURL, loc.SkyScannerID, loc.ThingsToDo, loc.FiveDayWPI)
 		if err != nil {
 			// Rollback the transaction in case of an error
@@ -135,29 +108,5 @@ func InsertLocationData(destinationDBPath string, records []WeatherRecord) error
 	return nil
 }
 
-// getUniqueLocations returns a list of unique locations from the given weather records, maintaining order
-func getUniqueLocations(records []WeatherRecord) []Location {
-	uniqueMap := make(map[string]struct{})
-	var uniqueLocations []Location
-
-	for _, record := range records {
-		key := record.CityName + record.CountryCode
-		if _, exists := uniqueMap[key]; !exists {
-			uniqueMap[key] = struct{}{}
-			uniqueLocations = append(uniqueLocations, Location{
-				CityName:    record.CityName,
-				CountryCode: record.CountryCode,
-				IATA:        record.IATA, // Assuming IATA is same as city_name for simplicity
-				SkyScannerID: "placeholder_skyscanner_uuid",
-				AirbnbURL:   "placeholder_airbnb_url",
-				BookingURL:  "placeholder_booking_url",
-				ThingsToDo:  "placeholder_things_to_do",
-				FiveDayWPI:  0.0,
-			})
-		}
-	}
-
-	return uniqueLocations
-}
 
 
