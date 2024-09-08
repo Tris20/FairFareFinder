a1af689bd500bbfa6e11879ff40a0e88dde1b881
Tristan <tris.robinson90@gmail.com>
2024-09-08 17:42:57 +0200

    FIX: typo in webpages and pathing

---
+++ src/backend/web_pages/glasgow.go
@@ -0,0 +1,19 @@
+package fffwebpages
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// handles requests to the forecast page
+func PresentGlasgowFlightDestinations(w http.ResponseWriter, r *http.Request) {
+	// serving a static file
+	pageContent, err := ioutil.ReadFile("src/frontend/html/glasgow-flight-destinations.html")
+	if err != nil {
+		log.Printf("Error reading forecast page file: %v", err)
+		http.Error(w, "Internal server error", 500)
+		return
+	}
+	w.Write(pageContent)
+}
