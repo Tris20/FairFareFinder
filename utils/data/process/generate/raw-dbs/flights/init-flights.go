3648dc9458347f7988488e2bf0306d9cb3e92cc1
Tristan <tris.robinson90@gmail.com>
2024-09-11 00:36:10 +0200

    DEV: adding city and country to prices fetch

--- utils/data/process/generate/raw-dbs/flights/init-flights.go
+++ utils/data/process/generate/raw-dbs/flights/init-flights.go
@@ -2,49 +2,63 @@
 package main
 
 import (
-	"database/sql"
-	"log"
+    "bufio"
+    "database/sql"
+    "fmt"
+    "log"
+    "os"
 
-	_ "github.com/mattn/go-sqlite3"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// Open the SQLite database.
-	db, err := sql.Open("sqlite3", "../../../../../../data/raw/flights/flights.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+    // Open the SQLite database.
+    db, err := sql.Open("sqlite3", "../../../../../../data/raw/flights/flights.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
 
-	_, err = db.Exec(`CREATE TABLE "schedule" (
-	"id"	INTEGER,
-	"flightNumber"	TEXT NOT NULL,
-	"departureAirport"	TEXT,
-	"arrivalAirport"	TEXT,
-	"departureTime"	TEXT,
-	"arrivalTime"	TEXT,
-	"direction"	TEXT NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);`)
+    // Create the "schedule" table if it does not already exist.
+    _, err = db.Exec(`CREATE TABLE IF NOT EXISTS "schedule" (
+        "id"   INTEGER,
+        "flightNumber" TEXT NOT NULL,
+        "departureAirport" TEXT,
+        "arrivalAirport" TEXT,
+        "departureTime" TEXT,
+        "arrivalTime" TEXT,
+        "direction" TEXT NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT)
+    );`)
+    if err != nil {
+        log.Fatal(err)
+    } else {
+        log.Println("Checked/created 'schedule' table successfully.")
+    }
 
-if err != nil {
-		log.Fatal(err)
-	}
-log.Println("schedule table created successfully.")
+    // Prompt to continue
+    fmt.Println("Press 'Enter' to continue with the next table...")
+    bufio.NewReader(os.Stdin).ReadBytes('\n')
 
-
-	_, err = db.Exec(`CREATE TABLE "skyscannerprices" (
-	"origin_iata"	TEXT,
-  "origin"	TEXT,
-	"destination_iata"	TEXT,
-  "destination"	TEXT,
-	"this_weekend"	REAL,
-	"next_weekend"	REAL
-);`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-
-	log.Println("skyscannerprices table created successfully.")
+    // Create the "skyscannerprices" table if it does not already exist.
+    _, err = db.Exec(`
+    CREATE TABLE IF NOT EXISTS "skyscannerprices" (
+        "origin_city" TEXT,
+        "origin_country" TEXT,
+        "origin_iata" TEXT,
+        "origin_skyscanner_id" TEXT,
+        "destination_city" TEXT,
+        "destination_country" TEXT,
+        "destination_iata" TEXT,
+        "destination_skyscanner_id" TEXT,
+        "this_weekend" REAL,    
+        "next_weekend" REAL
+    );
+    `)
+    if err != nil {
+        log.Fatal(err)
+    } else {
+        log.Println("Checked/created 'skyscannerprices' table successfully.")
+    }
 }
+
