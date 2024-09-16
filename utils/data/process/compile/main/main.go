38ea6ecc08151c682fd9bd748b288242bf6a89d0
Tristan <tris.robinson90@gmail.com>
2024-09-16 13:50:42 +0200

    DEV: just some cleanup before closing compile branch

--- utils/data/process/compile/main/main.go
+++ utils/data/process/compile/main/main.go
@@ -44,28 +44,26 @@ func getCurrentTime() (time.Weekday, int) {
 func runAllTasks(relativeBase string) {
     green := "\033[32m"
     reset := "\033[0m"
+ 
 
     runExecutableInDir(filepath.Join(relativeBase, "fetch/flights/schedule"), "aerodatabox")
     fmt.Printf("%sCOMPLETED: aerodatabox (flight schedule)%s\n", green, reset)
 
     runExecutableInDir(filepath.Join(relativeBase, "fetch/flights/prices"), "prices")
     fmt.Printf("%sCOMPLETED: prices (flight prices)%s\n", green, reset)
-
     runExecutableInDir(filepath.Join(relativeBase, "fetch/weather"), "update-weather-db")
     fmt.Printf("%sCOMPLETED: update-weather-db (weather update)%s\n", green, reset)
 
     runExecutableInDir(filepath.Join(relativeBase, "process/calculate/weather"), "weather")
     fmt.Printf("%sCOMPLETED: weather (weather calculation)%s\n", green, reset)
-
     runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/flights"), "flights")
     fmt.Printf("%sCOMPLETED: flights (process compile)%s\n", green, reset)
-
     runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/weather"), "weather")
     fmt.Printf("%sCOMPLETED: process/compile/main/weather%s\n", green, reset)
 
     runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/locations"), "locations")
     fmt.Printf("%sCOMPLETED: process/compile/main/locations%s\n", green, reset)
-}
+  }
 
 // Function to run only compile tasks
 func runCompileTasks(relativeBase string) {
