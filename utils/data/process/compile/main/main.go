1aa695587519ca2301609af47caf389897d213a9
Tristan <tris.robinson90@gmail.com>
2024-09-21 22:24:04 +0200

    FEATURE: boxes are hyperlinked, mouseover makes clicking more apparent

--- utils/data/process/compile/main/main.go
+++ utils/data/process/compile/main/main.go
@@ -46,6 +46,7 @@ func runAllTasks(relativeBase string) {
     reset := "\033[0m"
  
 
+    // FETCH
     runExecutableInDir(filepath.Join(relativeBase, "fetch/flights/schedule"), "aerodatabox")
     fmt.Printf("%sCOMPLETED: aerodatabox (flight schedule)%s\n", green, reset)
 
@@ -53,9 +54,17 @@ func runAllTasks(relativeBase string) {
     fmt.Printf("%sCOMPLETED: prices (flight prices)%s\n", green, reset)
     runExecutableInDir(filepath.Join(relativeBase, "fetch/weather"), "update-weather-db")
     fmt.Printf("%sCOMPLETED: update-weather-db (weather update)%s\n", green, reset)
+    runExecutableInDir(filepath.Join(relativeBase, "fetch/accommocation/booking-com/get-properties"), "get-properties")
+    fmt.Printf("%sCOMPLETED: get-properties (properties update)%s\n", green, reset)
 
+
+
+    //Calculate
     runExecutableInDir(filepath.Join(relativeBase, "process/calculate/weather"), "weather")
     fmt.Printf("%sCOMPLETED: weather (weather calculation)%s\n", green, reset)
+ 
+   
+    //Compile
     runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/flights"), "flights")
     fmt.Printf("%sCOMPLETED: flights (process compile)%s\n", green, reset)
     runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/weather"), "weather")
@@ -63,7 +72,18 @@ func runAllTasks(relativeBase string) {
 
     runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/locations"), "locations")
     fmt.Printf("%sCOMPLETED: process/compile/main/locations%s\n", green, reset)
-  }
+
+   runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/accommodation/booking-com"), "booking-com")
+   fmt.Printf("%sCOMPLETED:  process/compile/main/accommodation/booking-com%s\n", green, reset)
+
+
+   // 5 nights and flights
+   runExecutableInDir(filepath.Join(relativeBase, "process/calculate/main/five-nights-and-flights"), "five-nights-and-flights")
+   fmt.Printf("%sCOMPLETED:  process/calculate/main/five-nights-and-flights%s\n", green, reset)
+
+
+
+ }
 
 // Function to run only compile tasks
 func runCompileTasks(relativeBase string) {
@@ -81,7 +101,17 @@ func runCompileTasks(relativeBase string) {
 
     runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/locations"), "locations")
     fmt.Printf("%sCOMPLETED: process/compile/main/locations%s\n", green, reset)
-}
+
+   runExecutableInDir(filepath.Join(relativeBase, "process/compile/main/accommodation/booking-com"), "booking-com")
+   fmt.Printf("%sCOMPLETED:  process/compile/main/accommodation/booking-com%s\n", green, reset)
+
+  // 5 nights and flights
+   runExecutableInDir(filepath.Join(relativeBase, "process/calculate/main/five-nights-and-flights"), "five-nights-and-flights")
+   fmt.Printf("%sCOMPLETED:  process/calculate/main/five-nights-and-flights%s\n", green, reset)
+
+
+
+ }
 
 
 
