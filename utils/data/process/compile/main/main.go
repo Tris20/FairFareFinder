d250909200deb54b0c26d3b271eec4cd21ee5e9c
Tristan <tris.robinson90@gmail.com>
2025-01-04 13:09:32 +0100

    FIX: hour set to 1 incorrectly

--- utils/data/process/compile/main/main.go
+++ utils/data/process/compile/main/main.go
@@ -295,7 +295,7 @@ func main() {
 			}
 
 			// Generate new db every 6 hours: 3 = 3am; 9am; 3pm; 9pm.
-			if currentHour%6 == 1 {
+			if currentHour%6 == 3 {
 				startTime := time.Now()
 				// Monday, 3am, Start a completely new new_main.db
 				if currentDay == time.Monday && currentHour == 3 {
